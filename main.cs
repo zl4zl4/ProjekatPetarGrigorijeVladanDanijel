a46966027f63866a413083bfe099be4f0fbb0f00
Петар Родић <>
2022-04-29 20:05:15 +0000

    Dodata neproverena metoda za proveru poziva na broj modelom 97

--- main.cs
+++ main.cs
@@ -1,7 +1,19 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+  public static ulong Model97()
+  {
+  ulong PozivNaBroj,PNB2Nule,KontrolniBroj;
+          Console.WriteLine("Unesi poziv na broj:");
+           PozivNaBroj=ulong.Parse(Console.ReadLine());
+          PNB2Nule=ulong.Parse(Convert.ToString(PozivNaBroj)+"00");
+        KontrolniBroj=98-PNB2Nule%97;
+          
+          return KontrolniBroj;
+  } 
+  public static void Main (string[] args) 
+  {
+        Console.ForegroundColor=ConsoleColor.Cyan;
+    Console.WriteLine ("Hello world");
   }
 }
